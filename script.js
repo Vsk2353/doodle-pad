6854d54e152763517990736c13ea6587fad29f72
Visky <65082238+Vsk2353@users.noreply.github.com>
2025-03-06 15:15:25 +0530

    Add custom grid size option

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let sketchPad = document.querySelector(".container");
 let bStart = document.querySelector("#one");
+let bChoice = document.querySelector("#two");
 
 bStart.addEventListener("click", function (){
     sketchPad.innerHTML="";
@@ -16,4 +17,27 @@ bStart.addEventListener("click", function (){
         grid.style.backgroundColor = "blue";
     })
 })
+})
+
+bChoice.addEventListener("click", function (){
+    let choice = prompt("Enter the number of squares per side for the new grid (min: 1, max:100)","");
+    sketchPad.innerHTML = "";
+    if(choice<1 || choice>100 || isNaN(choice)){
+        alert("Enter within the limits");
+        return;
+    }
+    for(let i=1;i<=choice*choice;i++){
+        let square = document.createElement("div");
+        square.classList.add("grid-square");
+        square.style.width = `calc(100%/${choice})`;
+        square.style.height = `calc(100%/${choice})`;
+        sketchPad.appendChild(square)
+    }
+    let grids = document.querySelectorAll(".grid-square");
+
+    grids.forEach(grid => {
+        grid.addEventListener("mouseenter", function (){
+        grid.style.backgroundColor = "blue";})
+})
+
 })
