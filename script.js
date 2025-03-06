3b7fed9475195f1ff910bf8f85a5f5fac2988a85
Visky <65082238+Vsk2353@users.noreply.github.com>
2025-03-06 14:44:46 +0530

    Implement logic to create default 16x16 grid

---
+++ script.js
@@ -0,0 +1,10 @@
+let sketchPad = document.querySelector(".container");
+let bStart = document.querySelector("#one");
+bStart.addEventListener("click", function (){
+    sketchPad.innerHTML="";
+    for(let i=1;i<=16*16;i++){
+        let square = document.createElement("div");
+        square.classList.add("grid-square");
+        sketchPad.appendChild(square);
+    }
+})
