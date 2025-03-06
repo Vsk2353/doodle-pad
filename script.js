8db2739fd288fd47711270c47e0e64072c281225
Visky <65082238+Vsk2353@users.noreply.github.com>
2025-03-06 14:59:07 +0530

    Add eventlistener to check for mouse hover and change color

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let sketchPad = document.querySelector(".container");
 let bStart = document.querySelector("#one");
+
 bStart.addEventListener("click", function (){
     sketchPad.innerHTML="";
     for(let i=1;i<=16*16;i++){
@@ -7,4 +8,12 @@ bStart.addEventListener("click", function (){
         square.classList.add("grid-square");
         sketchPad.appendChild(square);
     }
+
+    let grids = document.querySelectorAll(".grid-square");
+
+    grids.forEach(grid => {
+        grid.addEventListener("mouseenter", function (){
+        grid.style.backgroundColor = "blue";
+    })
+})
 })
